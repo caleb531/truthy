7d793f56e66e46a6101caff54b1e8da049360ad5
Caleb Evans <caleb@calebevans.me>
2016-12-28 20:33:27 -0800

    Add basic app structure and build config

---
+++ brunch-config.js
@@ -0,0 +1,32 @@
+// Brunch configuration
+// See http://brunch.io for documentation.
+'use strict';
+
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: {
+        'scripts/main.js': ['app/scripts/*.js', /^node_modules/],
+      }
+    },
+    stylesheets: {
+      joinTo: {
+        'styles/main.css': ['app/styles/main.scss']
+      }
+    }
+  },
+  modules: {
+    autoRequire: {
+      'scripts/main.js': ['scripts/main']
+    }
+  },
+  plugins: {
+    postcss: {
+      processors: [
+        require('autoprefixer')({
+          browsers: ['> 0.1%']
+        })
+      ]
+    }
+  }
+};
