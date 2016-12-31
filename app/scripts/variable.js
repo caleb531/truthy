4c6c21a4a37969ac62a1950b98f24fdb94823bcc
Caleb Evans <caleb@calebevans.me>
2016-12-31 11:18:09 -0800

    Update variable name when input value changes
    Also prevent users from entering an empty or non-alphanumeric name.

--- app/scripts/variable.js
+++ app/scripts/variable.js
@@ -6,11 +6,39 @@ var _ = require('underscore');
 function Variable(args) {
   this.name = args.name;
 }
+Variable.validNamePattern = /^[A-Za-z]$/;
 
 Variable.Component = {};
 
+Variable.Component.controller = function () {
+  return {
+    getVariableIndex: function (variableElem) {
+      var variableInputs = variableElem
+        .parentNode
+        .parentNode
+        .getElementsByClassName('variable');
+      var variableIndex = Array.prototype.indexOf.call(variableInputs, variableElem.parentNode);
+      return variableIndex;
+    },
+    updateVariableName: function (ctrl, app, event) {
+      var variableIndex = ctrl.getVariableIndex(event.target);
+      if (Variable.validNamePattern.test(event.target.value)) {
+        app.variables[variableIndex].name = event.target.value;
+      }
+    },
+    focusVariableInput: function (event) {
+      if (event.target.nodeName === 'INPUT') {
+        event.target.select();
+      }
+    }
+  };
+};
+
 Variable.Component.view = function (ctrl, app) {
-  return m('div#variables', _.map(app.variables, function (variable) {
+  return m('div#variables', {
+    onclick: ctrl.focusVariableInput,
+    oninput: _.partial(ctrl.updateVariableName, ctrl, app)
+  }, _.map(app.variables, function (variable) {
     return m('div.variable', m('input', {
       type: 'text',
       value: variable.name,
