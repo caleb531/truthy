92b92df387491f4fb641ae72723dfbe4af6ad7cc
Caleb Evans <caleb@calebevans.me>
2017-01-02 18:10:59 -0800

    Add tests for NOT operation

--- test/expresison.spec.js
+++ test/expresison.spec.js
@@ -12,4 +12,29 @@ describe('expression', function () {
     expect(expression).to.have.property('string', ' p and q ');
   });
 
+  function testExprs(args) {
+    // Generate a test for each supplied expression
+    args.exprStrings.forEach(function (exprString) {
+      var expression = new Expression({string: exprString});
+      it('should evaluate `' + exprString + '`', function () {
+        // Test each expression against the given permutations of variable
+        // values and the expected outputs
+        args.testCases.forEach(function (testCase) {
+          var actualOutput = expression.evaluate(testCase.varValues);
+          expect(actualOutput).to.equal(testCase.output);
+        });
+      });
+    });
+  }
+
+  describe('NOT operation', function () {
+    testExprs({
+      exprStrings: ['not p', 'not  p', 'NoT p', '!p', '!  p'],
+      testCases: [
+        {varValues: {p: false}, output: true},
+        {varValues: {p: true}, output: false}
+      ]
+    });
+  });
+
 });
