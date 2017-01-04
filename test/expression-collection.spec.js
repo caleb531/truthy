cf0f13ed9348716a62560f049c92fefcf6dcb524
Caleb Evans <caleb@calebevans.me>
2017-01-04 13:36:09 -0800

    Add spec for ExpressionCollection model

---
+++ test/expression-collection.spec.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var expect = require('chai').expect;
+var ExpressionCollection = require('../app/scripts/expression-collection');
+var Expression = require('../app/scripts/expression');
+
+describe('expression collection', function () {
+
+  it('should initialize with list of expressions', function () {
+    var expressions = new ExpressionCollection({
+      items: [{string: 'a xor b'}, {string: 's nand t'}]
+    });
+    expect(expressions).to.have.property('items');
+    expect(expressions.items).to.have.length(2);
+    expressions.items.forEach(function (expression) {
+      expect(expression).to.be.an.instanceof(Expression);
+    });
+  });
+
+  it('should serialize to a JSON object', function () {
+    var expressions = new ExpressionCollection({
+      items: [{string: 'a xor b'}, {string: 's nand t'}]
+    });
+    expect(expressions.serialize()).to.deep.equal({
+      items: [{string: 'a xor b'}, {string: 's nand t'}]
+    });
+  });
+
+});
