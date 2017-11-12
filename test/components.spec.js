be9bea1024960819495404da13bc8fb7e4bc52b8
Caleb Evans <caleb@calebevans.me>
2017-11-11 18:44:23 -0800

    Write first UI tests

--- test/components.spec.js
+++ test/components.spec.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var expect = require('chai').expect;
+var chai = require('chai');
+chai.use(require('chai-dom'));
+var expect = chai.expect;
 var m = require('mithril');
 var AppComponent = require('../app/scripts/components/app');
-var $ = require('jquery');
 
 describe('app UI', function () {
 
@@ -15,8 +16,26 @@ describe('app UI', function () {
     m.mount(document.body, null);
   });
 
-  it('should render app', function () {
-      expect($).to.be.ok;
+  it('should render default variables', function () {
+    var variables = document.querySelectorAll('div.variable input');
+    expect(variables).to.have.length(2);
+    expect(variables[0]).to.have.value('p');
+    expect(variables[1]).to.have.value('q');
   });
+
+  it('should render default expressions', function () {
+    var expressions = document.querySelectorAll('th.expression input');
+    expect(expressions).to.have.length(3);
+    expect(expressions[0]).to.have.value('not p');
+    expect(expressions[1]).to.have.value('p and q');
+    expect(expressions[2]).to.have.value('p or q');
+  });
+
+  // it('should add new variable', function () {
+  //   var variables = document.querySelectorAll('div.variable');
+  //   expect(variables).to.have.length(2);
+  //   variables[1].querySelector('.control-add').click();
+  //   expect(variables).to.have.length(3);
+  // });
 
 });
