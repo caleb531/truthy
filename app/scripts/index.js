1c3ce1e0b046add5c2d6034630dbcd2f878055bc
Caleb Evans <caleb@calebevans.me>
2020-01-01 16:58:22 -0800

    Remove FastClick indefinitely
    It seems to actually be inhibiting some mobile taps from taking effect.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,4 @@
 import m from 'mithril';
-import FastClick from 'fastclick';
 import AppComponent from './components/app.js';
 
 m.mount(document.querySelector('main'), AppComponent);
-FastClick.attach(document.body);
