0b38f40a846f9edf14be3a45e28237ff96fabdf7
Caleb Evans <caleb@calebevans.me>
2020-02-08 11:39:41 -0800

    Add AND/OR arithmetic operators to Reference Sidebar

--- app/scripts/components/reference.js
+++ app/scripts/components/reference.js
@@ -37,11 +37,11 @@ ReferenceComponent.features = [
   },
   {
     name: 'AND',
-    examples: ['p and q', 'p & q', 'p && q']
+    examples: ['p and q', 'p & q', 'p && q', 'p * q']
   },
   {
     name: 'OR',
-    examples: ['p or q', 'p | q', 'p || q']
+    examples: ['p or q', 'p | q', 'p || q', 'p + q']
   },
   {
     name: 'Parentheses',
