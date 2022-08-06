bf33bafd03eb1265a7b10468b4a9a74f80d5adca
Caleb Evans <caleb@calebevans.me>
2022-08-06 12:41:54 -0700

    Fix watching of stylesheets and static assets

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import path from 'path';
+import glob from 'glob';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
@@ -7,6 +9,20 @@ import copy from 'rollup-plugin-copy';
 import scss from 'rollup-plugin-scss';
 import serve from 'rollup-plugin-serve';
 
+// Watch additional files outside of the module graph (e.g. SCSS or static
+// assets); see <https://github.com/rollup/rollup/issues/3414>
+function watcher(globs) {
+  return {
+    buildStart() {
+      for (const item of globs) {
+        glob.sync(path.resolve(__dirname, item)).forEach((filename) => {
+          this.addWatchFile(filename);
+        });
+      }
+    }
+  };
+}
+
 export default {
   input: 'src/scripts/index.js',
   output: {
@@ -15,6 +31,7 @@ export default {
     exports: 'auto'
   },
   plugins: [
+    watcher(['src/styles/*.*', 'public/**/*.*']),
     copy({
       targets: [
         { src: 'public/*', dest: 'dist/' }
