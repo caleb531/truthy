05dcddb3cf917ecf7005112bea6049de6c3c0d56
Caleb Evans <caleb@calebevans.me>
2022-08-07 14:32:18 -0700

    Replace custom watcher code with rollup-plugin-watch-globs

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,12 @@
-import path from 'path';
-import glob from 'glob';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
+import watchGlobs from 'rollup-plugin-watch-globs';
 import peggy from 'rollup-plugin-peggy';
 import copy from 'rollup-plugin-copy';
 import scss from 'rollup-plugin-scss';
 import serve from 'rollup-plugin-serve';
-
-// Watch additional files outside of the module graph (e.g. SCSS or static
-// assets); see <https://github.com/rollup/rollup/issues/3414>
-function watcher(globs) {
-  return {
-    buildStart() {
-      for (const item of globs) {
-        glob.sync(path.resolve(__dirname, item)).forEach((filename) => {
-          this.addWatchFile(filename);
-        });
-      }
-    }
-  };
-}
 
 export default {
   input: 'src/scripts/index.js',
@@ -32,7 +17,7 @@ export default {
     exports: 'auto'
   },
   plugins: [
-    watcher(['src/styles/*.*', 'public/**/*.*']),
+    watchGlobs(['src/styles/*.*', 'public/**/*.*']),
     copy({
       targets: [
         { src: 'public/*', dest: 'dist/' }
