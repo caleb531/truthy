20066b2ab4d50a158db3591c0a7ffa65b19d33a2
Caleb Evans <caleb@calebevans.me>
2022-12-08 18:07:56 -0800

    Properly serve from /truthy/ for production

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import peggy from 'rollup-plugin-peggy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // By default, Vite will assume we are serving from the root of the domain
+  // (i.e. /); however, because we are serving Truthy from a subdirectory of my
+  // projects domain (e.g. https://projects.calebevans.me/truthy/), we must
+  // specify . as the base directory to serve from
+  base: './',
   plugins: [
     peggy({ cache: true })
   ],
