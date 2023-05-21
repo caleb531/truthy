a51f019842d23a8f86e36fc490758223b3408574
Caleb Evans <caleb@calebevans.me>
2023-05-21 15:52:12 -0700

    Switch from Google Fonts to @fontsource

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 import m from 'mithril';
 import AppComponent from './components/app.js';
 import '../styles/index.scss';
+import '@fontsource/ubuntu/300.css';
+import '@fontsource/ubuntu/400.css';
+import '@fontsource/oxygen-mono';
 
 m.mount(document.querySelector('main'), AppComponent);
